ae8b375e105af5f9193e0b3e962ccbafe1768839
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-04-19 19:36:43 +0200

    landing page and error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
-
+import img from '../assets/images/not-found.svg'
+import Warpper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
     const err = useRouteError();
     console.log(err);
+    if (err.status === 404) {
+        return (
+            <Warpper>
+                <div>
+                    <img src={img} alt="not found" />
+                    <h3> page not found</h3>
+                    <p>we can't seem to find the page you are looking for </p>
+                    <Link to='/dashboard'>back home</Link>
+                </div>
+            </Warpper>
+        )
+    }
 
-    
     return (
         <>
-            <div>Error</div>
-            <Link to='/'>Back  home</Link>
+            <Wrapper>
+                <div>
+                    <h3>something went wrong</h3>
+                </div>
+            </Wrapper>
         </>
     )
 }
